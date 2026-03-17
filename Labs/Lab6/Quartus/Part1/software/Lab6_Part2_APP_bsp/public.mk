ccd42a250960b44c3eaefcf425e45b02f8f989d6
WH039 <huang.weicheng.03@gmail.com>
2026-03-17 15:39:24 -0400

    lab6

--- Labs/Lab6/Quartus/Part1/software/Lab6_Part2_APP_bsp/public.mk
+++ Labs/Lab6/Quartus/Part1/software/Lab6_Part2_APP_bsp/public.mk
@@ -157,9 +157,9 @@ SOPC_SYSID_FLAG += --sidp=0x9020
 ELF_PATCH_FLAG  += --sidp 0x9020
 
 # The SOPC Timestamp 
-# setting SOPC_TIMESTAMP is 1773764561
-SOPC_SYSID_FLAG += --timestamp=1773764561
-ELF_PATCH_FLAG  += --timestamp 1773764561
+# setting SOPC_TIMESTAMP is 1773771334
+SOPC_SYSID_FLAG += --timestamp=1773771334
+ELF_PATCH_FLAG  += --timestamp 1773771334
 
 # Enable JTAG UART driver to recover when host is inactive causing buffer to 
 # full without returning error. Printf will not fail with this recovery. none 
