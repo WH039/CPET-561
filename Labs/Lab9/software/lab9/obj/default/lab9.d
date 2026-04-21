38a4e9880e16690995897474fbf4c3aa5ad80dbd
WH113 <huang.weicheng.03@gmail.com>
2026-04-21 06:58:40 -0400

    lab9

---
+++ Labs/Lab9/software/lab9/obj/default/lab9.d
@@ -0,0 +1,37 @@
+obj/default/lab9.o: lab9.c ../lab9_bsp//HAL/inc/io.h \
+ ../lab9_bsp//HAL/inc/alt_types.h system.h ../lab9_bsp/linker.h \
+ ../lab9_bsp//HAL/inc/alt_types.h ../lab9_bsp//HAL/inc/sys/alt_irq.h \
+ ../lab9_bsp//HAL/inc/nios2.h ../lab9_bsp/system.h \
+ ../lab9_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../lab9_bsp//drivers/inc/altera_avalon_timer.h \
+ ../lab9_bsp//HAL/inc/sys/alt_dev.h ../lab9_bsp//HAL/inc/sys/alt_llist.h \
+ ../lab9_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../lab9_bsp//HAL/inc/sys/alt_warning.h
+
+../lab9_bsp//HAL/inc/io.h:
+
+../lab9_bsp//HAL/inc/alt_types.h:
+
+system.h:
+
+../lab9_bsp/linker.h:
+
+../lab9_bsp//HAL/inc/alt_types.h:
+
+../lab9_bsp//HAL/inc/sys/alt_irq.h:
+
+../lab9_bsp//HAL/inc/nios2.h:
+
+../lab9_bsp/system.h:
+
+../lab9_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../lab9_bsp//drivers/inc/altera_avalon_timer.h:
+
+../lab9_bsp//HAL/inc/sys/alt_dev.h:
+
+../lab9_bsp//HAL/inc/sys/alt_llist.h:
+
+../lab9_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../lab9_bsp//HAL/inc/sys/alt_warning.h:
